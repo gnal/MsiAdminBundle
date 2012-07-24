34f46af80777c6601a205918c0febfc183551737
gnal <qwertypianox@gmail.com>
2012-07-24 14:22:05 -0400

    fix

--- Table/Column/TextColumn.php
+++ Table/Column/TextColumn.php
@@ -4,16 +4,6 @@ namespace Msi\Bundle\AdminBundle\Table\Column;
 
 class TextColumn extends BaseColumn
 {
-    public function render()
-    {
-        if ($this->options['edit'] === false)
-            $html = $this->value;
-        else
-            $html = '<a href="'.$this->admin->genUrl('edit', array('id' => $this->object->getId())).'">'.$this->value.'</a>';
-
-        return '<td>'.$html.'</td>';
-    }
-
     public function getDefaultOptions()
     {
         return array(
