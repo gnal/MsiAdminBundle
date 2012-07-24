a7b5a3923cb8a54621607c8a21a6b469fe3a3316
gnal <qwertypianox@gmail.com>
2012-07-24 14:23:42 -0400

    fix

--- Table/Column/DateColumn.php
+++ Table/Column/DateColumn.php
@@ -4,13 +4,6 @@ namespace Msi\Bundle\AdminBundle\Table\Column;
 
 class DateColumn extends BaseColumn
 {
-    public function render()
-    {
-        $html = $this->value->format($this->options['format']);
-
-        return '<td title="'.$this->options['format'].'">'.$html.'</td>';
-    }
-
     public function getDefaultOptions()
     {
         return array(
