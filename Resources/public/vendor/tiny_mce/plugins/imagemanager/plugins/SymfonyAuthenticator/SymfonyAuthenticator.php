ab7e1740eb44617067d5d1ae9a229435dae74b35
gnal <qwertypianox@gmail.com>
2012-07-24 19:46:07 -0400

    fix

---
+++ Resources/public/vendor/tiny_mce/plugins/imagemanager/plugins/SymfonyAuthenticator/SymfonyAuthenticator.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * SymfonyAuthenticatorImpl.php
+ *
+ *
+ * -> Assuming credential to upload is called "canuploadassets"
+ * -> assuming SF_ROOT_DIR/setup.php contains symfony environment setup, needed to use sfContext
+ *
+ * @author Ilia Kantor
+ */
+
+// assuming standard location for plugin
+
+require_once(dirname(__FILE__).'/../../../../../../../../config/ProjectConfiguration.class.php');
+require_once __DIR__.'/../app/AppKernel.php';
+
+/**
+ * This class is a Symfony authenticator implementation.
+ *
+ * @package MCImageManager.Authenticators
+ */
+class Moxiecode_SymfonyAuthenticator extends Moxiecode_ManagerPlugin {
+    /**#@+
+	 * @access public
+	 */
+
+	/**
+	 * Main constructor.
+	 */
+	function Moxiecode_SymfonyAuthenticator() {
+	}
+
+	function onAuthenticate(&$man) {
+        $kernel = new AppKernel('prod', false);
+        // $configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'prod', false);
+        // $context = sfContext::createInstance($configuration);
+        // return $context->getUser()->isAuthenticated();
+	}
+
+	/**#@-*/
+}
+
+// Add plugin to MCManager
+$man->registerPlugin("SymfonyAuthenticator", new Moxiecode_SymfonyAuthenticator());
+
