71a102643e054e996859ca2ae3c8eefabdf31dc3
gnal <qwertypianox@gmail.com>
2012-07-24 14:34:20 -0400

    fix

--- Table/Column/BooleanColumn.php
+++ Table/Column/BooleanColumn.php
@@ -16,9 +16,6 @@ class BooleanColumn extends BaseColumn
     public function getDefaultOptions()
     {
         return array(
-            'attr' => array(
-                'class' => 'span1',
-            ),
             'label_true' => '<span class="badge badge-success"><i class="icon-ok icon-white"><span class="hide">1</span></i></span>',
             'label_false' => '<span class="badge"><i class="icon-ok icon-white"><span class="hide">0</span></i></span>',
         );
