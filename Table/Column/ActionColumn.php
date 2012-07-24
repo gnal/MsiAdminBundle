cb5af17f5dc4c576e53d0ca6bc2500aeae3efeb3
gnal <qwertypianox@gmail.com>
2012-07-24 14:05:36 -0400

    what

--- Table/Column/ActionColumn.php
+++ Table/Column/ActionColumn.php
@@ -4,44 +4,6 @@ namespace Msi\Bundle\AdminBundle\Table\Column;
 
 class ActionColumn extends BaseColumn
 {
-    // public function render()
-    // {
-    //     $html = '<td class="text-right">';
-    //     if (isset($this->options['actions'])) {
-    //         foreach ($this->options['actions'] as $k => $v) {
-    //             $html .= '<a class="btn btn-mini" href="'.$this->admin->genUrl($v, array('id' => $this->object->getId())).'">'.$k.'</a> ';
-    //         }
-    //     }
-
-    //     if ($this->admin->hasChild())
-    //         $html .= '<a class="btn btn-mini" href="'.$this->admin->getChild()->genUrl('index', array('parentId' => $this->object->getId())).'">'.$this->options['nested']['label'].'</a> ';
-
-    //     $html .= $this->renderEdit();
-    //     $html .= $this->renderDelete();
-
-    //     $html .= '</td>';
-
-    //     return $html;
-    // }
-
-    // public function renderEdit()
-    // {
-    //     $html = '';
-
-    //     $html .= '<a class="btn btn-mini" href="'.$this->admin->genUrl('edit', array('id' => $this->object->getId())).'">'.$this->options['edit']['label'].'</a> ';
-
-    //     return $html;
-    // }
-
-    // public function renderDelete()
-    // {
-    //     $html = '';
-
-    //     $html .= '<a class="action-delete btn btn-mini" href="#" data-url="'.$this->admin->genUrl('delete', array('id' => $this->object->getId())).'">'.$this->options['delete']['label'].'</a> ';
-
-    //     return $html;
-    // }
-
     public function getDefaultOptions()
     {
         return array(
