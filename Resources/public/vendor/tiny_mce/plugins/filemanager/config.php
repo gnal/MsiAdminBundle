e0f165f7a72f4ec60a234fae54ae2cb887d2fcba
gnal <qwertypianox@gmail.com>
2012-08-07 20:04:10 -0400

    fixing tiny mce authentication

--- Resources/public/vendor/tiny_mce/plugins/filemanager/config.php
+++ Resources/public/vendor/tiny_mce/plugins/filemanager/config.php
@@ -110,7 +110,7 @@
 	$mcFileManagerConfig['unzip.allow_override'] = "*";
 
 	// Authenication
-	$mcFileManagerConfig['authenticator'] = "SessionAuthenticator";
+	$mcFileManagerConfig['authenticator'] = "SymfonyAuthenticator";
 	$mcFileManagerConfig['authenticator.login_page'] = "login_session_auth.php";
 	$mcFileManagerConfig['authenticator.allow_override'] = "*";
 
