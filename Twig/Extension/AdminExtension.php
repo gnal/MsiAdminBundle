f12305f751077ead5bdc93654f6a131ee77ef781
gnal <qwertypianox@gmail.com>
2012-10-12 11:21:46 -0400

    fix

---
+++ Twig/Extension/AdminExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Msi\Bundle\AdminBundle\Twig\Extension;
+
+class AdminExtension extends \Twig_Extension
+{
+    private $environment;
+
+    private $container;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'msi_admin_is_image' => new \Twig_Function_Method($this, 'isImage', array('is_safe' => array('html'))),
+        );
+    }
+
+    public function initRuntime(\Twig_Environment $environment)
+    {
+        $this->environment = $environment;
+    }
+
+    public function getName()
+    {
+        return 'msi_admin';
+    }
+
+    public function isImage($filename)
+    {
+        $handle = imagecreatefromstring($filename);
+
+        return $handle ? true : false;
+    }
+}
