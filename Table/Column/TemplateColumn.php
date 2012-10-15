84f8e8672dc1937bd4ccdf4d1597fcce9dbfea8b
gnal <qwertypianox@gmail.com>
2012-10-15 10:20:28 -0400

    adding template column

---
+++ Table/Column/TemplateColumn.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Msi\Bundle\AdminBundle\Table\Column;
+
+class TemplateColumn extends BaseColumn
+{
+    public function getDefaultOptions()
+    {
+        return array();
+    }
+}
